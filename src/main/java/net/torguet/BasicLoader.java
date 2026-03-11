d0ea3d6fc321f4e32a13c964c3948901237823c0
Patrice Torguet <torguet@gmail.com>
2026-03-11 09:17:19 +0100

    Converting tap2bas

--- src/main/java/net/torguet/BasicLoader.java
+++ src/main/java/net/torguet/BasicLoader.java
@@ -26,12 +26,12 @@ public class BasicLoader {
     }
 
 
-    void tap2Bas(String inputFile, String destFile) throws IOException {
+    private static void tap2Bas(String inputFile, String destFile) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(inputFile);
-        DataOutputStream fileOutputStream = null;
+        PrintStream fileOutputStream = null;
         if ((destFile != null) && (!destFile.isEmpty()))
         {
-            fileOutputStream = new DataOutputStream(new FileOutputStream(destFile));
+            fileOutputStream = new PrintStream(new FileOutputStream(destFile));
         }
         if (fileOutputStream == null)
         {
@@ -53,28 +53,43 @@ public class BasicLoader {
         while (buffer[i++]>0) {
 
         }
-        while (buffer[i]>0 || buffer[i+1]>0)
+        while (buffer[i]!=0 || buffer[i+1]!=0)
         {
             i+=2;
-            fileOutputStream.writeShort(buffer[i]+(buffer[i+1]<<8));
+            int lineNumber = buffer[i];
+            if (lineNumber < 0) {
+                lineNumber = 256 + lineNumber;
+            }
+            int lineNumberHigh = buffer[i+1];
+            if (lineNumberHigh < 0) {
+                lineNumberHigh = 256 + lineNumberHigh;
+            }
+            lineNumberHigh = lineNumberHigh << 8;
+            lineNumber = lineNumber+lineNumberHigh;
+            fileOutputStream.print(" "+lineNumber+" ");
             i+=2;
-            short car;
-            while ((car=buffer[i++])>0)
+            int car;
+            while (buffer[i]!=0)
             {
+                car=buffer[i];
+                if (car < 0)
+                    car = 256 + car;
+
                 if (car<128)
-                    fileOutputStream.write(car);
+                    fileOutputStream.print((char) car);
                 else
                 if (car < 247)
                 {
-                    fileOutputStream.writeUTF(BasicProgram.getKeywords()[car - 128]);
+                    fileOutputStream.print(BasicProgram.getKeywords()[car - 128]);
                 }
                 else
                 {
                     // Probably corrupted listing
                     // 247 : NEXT WITHOUT FOR
-                    fileOutputStream.writeUTF("CORRUPTED_ERROR_CODE");
-                    fileOutputStream.writeShort(car);
+                    fileOutputStream.print("CORRUPTED_ERROR_CODE");
+                    fileOutputStream.print(car);
                 }
+                i++;
             }
             fileOutputStream.write('\r');
             fileOutputStream.write('\n');
@@ -84,7 +99,8 @@ public class BasicLoader {
     }
 
     public static void main(String[] args) throws Exception {
-        BasicLoader basicLoader = new BasicLoader("src/main/resources/intro.bas");
+        BasicLoader.tap2Bas("src/main/resources/intro.tap", "src/main/resources/intro2.bas");
+        BasicLoader basicLoader = new BasicLoader("src/main/resources/intro2.bas");
         basicLoader.readProgram();
         BasicProgram basicProgram = basicLoader.getBasicProgram();
         basicProgram.getSortedLines().forEach(System.out::println);
